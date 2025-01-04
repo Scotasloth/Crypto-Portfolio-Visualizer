96ee79cb658eddc54e72daabeba860b51dcc0c82
Scotasloth <rossmcc76@gmail.com>
2025-01-04 19:33:36 +0000

    Function to add to DB added

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def subWindow(root, index):
         amountEntry = CTkEntry(master=addWin, placeholder_text="How much are you adding?", textvariable=amount).pack(pady=10)
         valueEntry = CTkEntry(master=addWin, placeholder_text="What is its current value?", textvariable=value).pack(pady=10)
 
-        enterBtn = CTkButton(master=addWin, text="Enter").pack(pady=10)
+        enterBtn = CTkButton(master=addWin, text="Enter", command=lambda: addData(cryptoName.get(), amount.get(), value.get())).pack(pady=10)
 
     elif index == 2:
         upWin = CTkToplevel(master=root)
@@ -49,7 +49,7 @@ def subWindow(root, index):
         amountEntry = CTkEntry(master=upWin, placeholder_text="How much are you adding?", textvariable=amount).pack(pady=10)
         valueEntry = CTkEntry(master=upWin, placeholder_text="What is its current value?", textvariable=value).pack(pady=10)
 
-        enterBtn = CTkButton(master=upWin, text="Enter").pack(pady=10)
+        enterBtn = CTkButton(master=upWin, text="Enter", command=lambda: updateData(cryptoName.get(), amount.get(), value.get())).pack(pady=10)
     
     elif index == 3:
         delWin = CTkToplevel(master=root)
@@ -58,19 +58,35 @@ def subWindow(root, index):
 
         coinEntry = CTkEntry(master=delWin, placeholder_text="Enter the name of the Crypto", textvariable=cryptoName).pack(pady=10)
 
-        enterBtn = CTkButton(master=delWin, text="Enter").pack(pady=10)
+        enterBtn = CTkButton(master=delWin, text="Enter", command=lambda: delData(cryptoName.get())).pack(pady=10)
 
-def addData():
-    return
+def addData(crypto, amount, val):
+    cursor = conn.cursor()
 
-def updateData():
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS portfolio (
+        id INTEGER PRIMARY KEY,
+        crypto TEXT,
+        amount INTEGER,
+        value INTEGER
+    )
+    ''')
+
+    cursor.execute('''
+        INSERT INTO portfolio (crypto, amount, value) VALUES (?, ?, ?)
+        ''', (crypto, amount, val))
+    
+    conn.commit()
+    conn.close()
+
+def updateData(crypto, amount, val):
     return
 
 def readData():
     return
 
-def delData():
-    return
+def delData(crypto):
+    print(crypto)
 
 if __name__ == "__main__":
    main()
