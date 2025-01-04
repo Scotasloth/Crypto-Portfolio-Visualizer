38599ab5ad97e9c2be2596706107a76a982a6f20
Scotasloth <rossmcc76@gmail.com>
2025-01-04 18:54:17 +0000

    Made basic UI and skeleton functions

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
-import customtkinter
+from customtkinter import *
 import matplotlib
 import numpy
+import sqlite3
 
 def main():
+    root = CTk()
+    root.title("Crypto Visualizer")
+    root.geometry("400x300")
+
+    addBtn = CTkButton(master=root, text="Add New Crypto", command=lambda: addData()).pack(pady=10)
+    updateBtn = CTkButton(master=root, text="Update", command=lambda: updateData()).pack(pady=10)
+    removeBtn = CTkButton(master=root, text="Remove", command=lambda: delData()).pack(pady=10)
+    pieBtn = CTkButton(master=root, text="Make Pie Chart", command=lambda: pieChart()).pack(pady=10)
+    lineBtn = CTkButton(master=root, text="Make Linegraph", command=lambda: lineGraph()).pack(pady=10)
+
+    root.mainloop()
+
+def pieChart():
+    return
+
+def lineGraph():
+    return
+
+def addData():
+    return
+
+def updateData():
+    return
+
+def readData():
+    return
+
+def delData():
     return
 
 if __name__ == "__main__":
